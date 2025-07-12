f2d665116d943ed418cc5dc3311e3f031f1a37b5
Hamad Ahmad Ansari <hamad.ansarif90@gmail.com>
2025-07-13 01:38:30 +0530

    feat: Add Authentication, Backend and Database using Clerk and Convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="m-10">
+
+      <SignInButton>
+        <Button>
+          Sign In
+        </Button>
+      </SignInButton>
     </div>
   );
 }
